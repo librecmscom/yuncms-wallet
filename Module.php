f1aee62331e693a50b8f71c5c8b6fea418bd65d7
Peter <xutongle@gmail.com>
2017-12-21 13:07:31 +0800

    钱包

---
+++ Module.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\wallet;
+
+use Yii;
+
+class Module extends \yii\base\Module
+{
+    /**
+     * 初始化
+     */
+    public function init()
+    {
+        parent::init();
+        $this->registerTranslations();
+    }
+
+    /**
+     * 注册语言包
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        if (!isset(Yii::$app->i18n->translations['wallet*'])) {
+            Yii::$app->i18n->translations['wallet*'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'sourceLanguage' => 'en-US',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+    }
+}
